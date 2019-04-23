cbc5062e47aac8558b4e590ade378b17970d5465
Kish Siva <rodes88@msn.com>
2019-04-23 19:49:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,17 +55,17 @@ class Song
   def self.genre_count 
     @@genre_count = {}
     count = 0
-    @@genre_count.each do |genre, count| 
-      genre << @genre[count]
-      count << @@genres.count(@genre[count])
-      count += 1 
-    end 
-    
-  def num_counter @@genres
-    nums.inject(Hash.new(0)) do |hash, e|
+    #@@genre_count.each do |genre, count| 
+     # genre << @genre[count]
+      #count << @@genres.count(@genre[count])
+      #count += 1 
+    #end 
+  def num_counter 
+    @@genres.inject(Hash.new(0)) do |hash, e|
     hash[e] += 1
     hash
   end
+  binding.pry
 end
     
     binding.pry
